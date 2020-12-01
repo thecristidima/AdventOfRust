fa6f6621252ee4c1e0bb32e3304f3dd31c22934c
Cristi Dima <thecristidima@gmail.com>
2020-12-02 01:17:49 +0200

    Tidy up day 1

---
+++ 2020/src/day-1/expenses.rs
@@ -0,0 +1,55 @@
+use std::collections::HashSet;
+
+use utils::files::read_lines_iter;
+
+pub fn part_one(input_path: &str, target_sum: i32) {
+    let mut set = HashSet::<i32>::new();
+
+    for line in read_lines_iter(input_path) {
+        let num = line
+            .expect("line exists")
+            .parse::<i32>()
+            .expect("line is number");
+
+        let target = target_sum - num;
+
+        if set.contains(&target) {
+            println!("[UGLY] Solution for part 1 is: {}", num * target); // 921504
+            break;
+        } else {
+            set.insert(num);
+        }
+    }
+}
+
+pub fn part_two(input_path: &str, target_sum: i32) {
+    let mut list = Vec::<(i32, i32, i8)>::new();
+
+    for line in read_lines_iter(input_path) {
+        let num = line
+            .expect("line exists")
+            .parse::<i32>()
+            .expect("line is number");
+
+        let target = target_sum - num;
+
+        let mut new_entries = Vec::<(i32, i32, i8)>::new();
+        for entry in &list {
+            match entry {
+                (a, b, 2) => {
+                    if a + b == target {
+                        println!("[UGLY] Solution for part 2 is {}", a * b * num); // 195700142
+                        return;
+                    }
+                }
+                (a, _, 1) => {
+                    new_entries.push((*a, num, 2));
+                }
+                _ => {}
+            }
+        }
+
+        list.push((num, 0, 1)); // (first_num, second_num, entries_in_tuple)
+        list.append(&mut new_entries);
+    }
+}
--- 2020/src/day-1/main.rs
+++ 2020/src/day-1/main.rs
@@ -1,60 +1,4 @@
-use std::collections::HashSet;
-
-use utils::files::read_lines_iter;
-
-fn part_1() {
-    let sum = 2020;
-    let mut set = HashSet::<i32>::new();
-
-    for line in read_lines_iter("src/day-1/input.txt") {
-        let num = line
-            .expect("line exists")
-            .parse::<i32>()
-            .expect("line is number");
-
-        let target = sum - num;
-
-        if set.contains(&target) {
-            println!("Solution for part 1 is: {}", num * target); // 921504
-            break;
-        } else {
-            set.insert(num);
-        }
-    }
-}
-
-fn part_2() {
-    let sum = 2020;
-    let mut list = Vec::<(i32, i32, i8)>::new();
-
-    for line in read_lines_iter("src/day-1/input.txt") {
-        let num = line
-            .expect("line exists")
-            .parse::<i32>()
-            .expect("line is number");
-
-        let target = sum - num;
-
-        let mut new_entries = Vec::<(i32, i32, i8)>::new();
-        for entry in &list {
-            match entry {
-                (a, b, 2) => {
-                    if a + b == target {
-                        println!("Solution for part 2 is {}", a * b * num); // 195700142
-                        return;
-                    }
-                }
-                (a, _, 1) => {
-                    new_entries.push((*a, num, 2));
-                }
-                _ => {}
-            }
-        }
-
-        list.push((num, 0, 1)); // (first_num, second_num, entries_in_tuple)
-        list.append(&mut new_entries);
-    }
-}
+mod expenses;
 
 /**
  * Part 1:
@@ -63,9 +7,10 @@ fn part_2() {
  *
  * Part 2:
  * 	Using the same list, find the _three_ expenses that add up to the same target.
- * 	Return their product
+ * 	Return their product.
+ *
  */
 fn main() {
-    part_1();
-    part_2();
+    expenses::part_one("src/day-1/input.txt", 2020); // 921504
+    expenses::part_two("src/day-1/input.txt", 2020); // 195700142
 }
