9f69165196e829130cc7f4ba6fe454ff8139de9e
Cristi Dima <thecristidima@gmail.com>
2024-12-24 00:33:05 +0200

    06.2024

--- 2024/src/day6.rs
+++ 2024/src/day6.rs
@@ -1,51 +1,38 @@
 use std::{
+    collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
 };
 
 pub fn solve() -> (i32, i32) {
     let mut map = GuardMap::from_file("inputs/day6.demo");
-    (part_one(&map), part_two(&mut map))
+    (part_one(&mut map), part_two(&mut map))
 }
 
-fn part_one(map: &GuardMap) -> i32 {
-    let mut map = map.clone();
+fn part_one(map: &mut GuardMap) -> i32 {
     map.compute_guard_path();
     map.count_visited()
 }
 
 fn part_two(map: &mut GuardMap) -> i32 {
-    // I'm not doing this one, it's dumb
-    -1
-}
-
-#[derive(Clone, Debug, PartialEq)]
-enum MapPoint {
-    Free,
-    Visited,
-    Obstacle,
-}
-
-#[derive(Clone, Debug)]
-enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
+    map.reset();
+    map.try_create_loop()
 }
 
 #[derive(Clone, Debug)]
 struct GuardMap {
     map: Vec<Vec<MapPoint>>,
-    direction: Direction,
+    start_direction: Direction,
+    crt_direction: Direction,
     start_pos: (usize, usize),
     crt_pos: (usize, usize),
+    obstacle_set: HashSet<(Direction, (usize, usize))>,
 }
 
 impl GuardMap {
     pub fn from_file(path: &str) -> Self {
         let mut map = Vec::<Vec<MapPoint>>::new();
-        let mut direction: Direction = Direction::Up;
+        let mut start_direction: Direction = Direction::Up;
         let mut start_pos: (usize, usize) = (0, 0);
 
         let reader = BufReader::new(File::open(path).unwrap());
@@ -61,7 +48,7 @@ impl GuardMap {
                 } else {
                     map_row.push(MapPoint::Free);
                     start_pos = (row, col);
-                    direction = match symbol {
+                    start_direction = match symbol {
                         '^' => Direction::Up,
                         '>' => Direction::Right,
                         'v' => Direction::Down,
@@ -75,75 +62,92 @@ impl GuardMap {
 
         GuardMap {
             map,
-            direction,
+            start_direction,
+            crt_direction: start_direction,
             start_pos,
             crt_pos: start_pos,
+            obstacle_set: HashSet::new(),
         }
     }
 
-    pub fn compute_guard_path(&mut self) {
+    pub fn compute_guard_path(&mut self) -> bool {
         loop {
             let row = self.crt_pos.0;
             let col = self.crt_pos.1;
             self.map[row][col] = MapPoint::Visited;
 
-            if !self.advance() {
-                println!("Exited the map at {:?}", self.crt_pos);
-                break;
+            match self.advance() {
+                AdvanceResult::Advanced => continue,
+                AdvanceResult::HitObstacle => {
+                    if self
+                        .obstacle_set
+                        .contains(&(self.crt_direction, self.crt_pos))
+                    {
+                        return false;
+                    }
+
+                    self.obstacle_set.insert((self.crt_direction, self.crt_pos));
+                    self.change_direction();
+                }
+                AdvanceResult::ExitedMap => {
+                    return true;
+                }
             }
         }
     }
 
-    fn advance(&mut self) -> bool {
+    fn advance(&mut self) -> AdvanceResult {
         let row = self.crt_pos.0;
         let col = self.crt_pos.1;
         let last_row = self.map.len() - 1;
         let last_col = self.map[0].len() - 1;
 
-        match self.direction {
+        match self.crt_direction {
             Direction::Up => {
                 if row == 0 {
-                    return false;
+                    AdvanceResult::ExitedMap
                 } else if self.map[row - 1][col] == MapPoint::Obstacle {
-                    self.change_direction();
+                    AdvanceResult::HitObstacle
                 } else {
                     self.crt_pos = (row - 1, col);
+                    AdvanceResult::Advanced
                 }
             }
             Direction::Right => {
                 if col == last_col {
-                    return false;
+                    AdvanceResult::ExitedMap
                 } else if self.map[row][col + 1] == MapPoint::Obstacle {
-                    self.change_direction();
+                    AdvanceResult::HitObstacle
                 } else {
                     self.crt_pos = (row, col + 1);
+                    AdvanceResult::Advanced
                 }
             }
             Direction::Down => {
                 if row == last_row {
-                    return false;
+                    AdvanceResult::ExitedMap
                 } else if self.map[row + 1][col] == MapPoint::Obstacle {
-                    self.change_direction();
+                    AdvanceResult::HitObstacle
                 } else {
                     self.crt_pos = (row + 1, col);
+                    AdvanceResult::Advanced
                 }
             }
             Direction::Left => {
                 if col == 0 {
-                    return false;
+                    AdvanceResult::ExitedMap
                 } else if self.map[row][col - 1] == MapPoint::Obstacle {
-                    self.change_direction();
+                    AdvanceResult::HitObstacle
                 } else {
                     self.crt_pos = (row, col - 1);
+                    AdvanceResult::Advanced
                 }
             }
         }
-
-        true
     }
 
     fn change_direction(&mut self) {
-        self.direction = match self.direction {
+        self.crt_direction = match self.crt_direction {
             Direction::Up => Direction::Right,
             Direction::Right => Direction::Down,
             Direction::Down => Direction::Left,
@@ -163,4 +167,75 @@ impl GuardMap {
         }
         ret
     }
+
+    pub fn try_create_loop(&mut self) -> i32 {
+        let mut ret = 0;
+
+        for row in 0..self.map.len() {
+            for col in 0..self.map[0].len() {
+                if (row, col) == self.start_pos {
+                    continue; // can't place obstacle on guard (starting position)
+                }
+
+                if self.map[row][col] == MapPoint::Free {
+                    self.map[row][col] = MapPoint::Obstacle;
+                    if !self.compute_guard_path() {
+                        ret += 1;
+                    }
+                    self.reset();
+                    self.map[row][col] = MapPoint::Free;
+                }
+            }
+        }
+
+        ret
+    }
+
+    fn reset(&mut self) {
+        for row in 0..self.map.len() {
+            for col in 0..self.map[0].len() {
+                if self.map[row][col] == MapPoint::Visited {
+                    self.map[row][col] = MapPoint::Free;
+                }
+            }
+        }
+        self.crt_direction = self.start_direction;
+        self.crt_pos = self.start_pos;
+        self.obstacle_set = HashSet::new();
+    }
+
+    #[allow(dead_code)]
+    fn print_map(&self) {
+        for row in 0..self.map.len() {
+            for col in 0..self.map[0].len() {
+                match self.map[row][col] {
+                    MapPoint::Free => print!("."),
+                    MapPoint::Visited => print!("|"),
+                    MapPoint::Obstacle => print!("#"),
+                };
+            }
+            println!();
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum MapPoint {
+    Free,
+    Visited,
+    Obstacle,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+enum AdvanceResult {
+    Advanced,
+    HitObstacle,
+    ExitedMap,
 }
